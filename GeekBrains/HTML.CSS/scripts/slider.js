2fbf27f402a0fa445173fa8d245bbb3dd754a8b9
Rendar <podminlgom.alex@yandex.ru>
2020-06-28 00:11:44 +0300

    start use git

---
+++ GeekBrains/HTML.CSS/scripts/slider.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function hidden() {
+    let slider = document.querySelector('.windows8');
+    console.log(slider);
+    slider.classList.add('hidden');
+});
+let sliderImg = document.querySelectorAll('.img-product-a-img:not(.hidden-max)');
+console.log(sliderImg);
